cfdbe0e9cde8cc2a7a9332069cf7438ed04d8f81
Reuben Thomas <rrt@sc3d.org>
2018-02-07 11:24:34 +0000

    Add missing require of cl to dtrt-indent-test.el

--- dtrt-indent-test.el
+++ dtrt-indent-test.el
@@ -21,7 +21,10 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301 USA
 
+(eval-when-compile (require 'cl))
+
 (require 'dtrt-indent)
+
 
 ;;-----------------------------------------------------------------
 ;; Tests
