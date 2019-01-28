9ab9cb9d7f391fb09f61c9289c51c36374ddbcbb
Reuben Thomas <rrt@sc3d.org>
2019-01-28 21:01:56 +0000

    dtrt-indent.el: bump version to 0.8 & add myself as Maintainer

--- dtrt-indent.el
+++ dtrt-indent.el
@@ -1,10 +1,11 @@
 ;;; dtrt-indent.el --- Adapt to foreign indentation offsets
 
 ;; Copyright (C) 2003, 2007, 2008 Julian Scheid
-;; Copyright (C) 2014-2018 Reuben Thomas
+;; Copyright (C) 2014-2019 Reuben Thomas
 
 ;; Author: Julian Scheid <julians37@googlemail.com>
-;; Version: 0.7
+;; Maintainer: Reuben Thomas <rrt@sc3d.org>
+;; Version: 0.8
 ;; Keywords: convenience files languages c
 
 ;; This file is free software; you can redistribute it and/or modify
