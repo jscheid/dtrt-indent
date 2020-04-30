50c440c80e0d15303d8ab543bce4c56e9c2bf407
Reuben Thomas <rrt@sc3d.org>
2020-04-30 11:23:02 +0100

    Bump version to 1.2

--- dtrt-indent.el
+++ dtrt-indent.el
@@ -5,7 +5,7 @@
 
 ;; Author: Julian Scheid <julians37@googlemail.com>
 ;; Maintainer: Reuben Thomas <rrt@sc3d.org>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: convenience files languages c
 
 ;; This file is free software; you can redistribute it and/or modify
